c78e2681d5ee9c7ea466f5d2f83998619ba7193c
Tshifhiwa <tshifhiwamphoshomali@gmail.com>
2021-01-10 17:35:25 +0200

    version 1.0.1

--- Tutor me/javascript/normalUsers/profile_pageLoad.js
+++ Tutor me/javascript/normalUsers/profile_pageLoad.js
@@ -11,61 +11,13 @@ function pageLoad(){
 		if(windowWidth >=1024)
 		{
 			informationContainer_div.style.marginLeft="300px";
-
         }
         else{
             informationContainer_div.style.marginLeft="0px";
         }
-        hideControls();
-
-
 	}
 	catch(ex)
 	{
-		alert(ex);
-	}
-}
-//Function for hiding some controls
-function hideControls(){
-	try{
-		var user_avatar = document.getElementById("user_avatar");
-		user_avatar.style.visibility = "hidden";
-	}
-	catch(ext){
-		console.log(ext);
-	}
-}
-function singleHider(control){
-	try{
-		var variable = document.getElementById(control);
-		
-	}
-	catch(ex){
-		console.log(ex);
-	}
-}
-//Unhide controls
-function unHide(control){
-	try{
-		var cont = document.getElementById(control);
-		cont.style.visibility = "visible";
-		cont.style.transition = "visibility 1s ease-in-out";
-	}
-	catch(ex){
-		console.log(ex);
-	}
-}
-function changeProfilePicture(){
-	try{
-		var user_Avatar_container = document.getElementById("uploadNewImage");
-
-		//Styling
-		
-		user_Avatar_container.style.backgroundColor = "rgb(174,170,173)";
-		
-
-	}
-	catch(ex){
 		alert(ex);
 	}
 }
